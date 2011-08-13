42f5323bde37bbc117edd97dc11f8fa8f13b70da
Chris Bielinski <chris@shadowreactor.com>
2011-08-13 12:20:04 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shadowreactor::Application.config.secret_token = 'cf4615366c6323f134beff7f9097394d316db1d38f80d1d3cd249f96e37c14888fe6608b3ee8f2118728b613c6026f2bc1ea5062247276b671c68934b7b55b21'
